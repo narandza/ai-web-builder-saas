0b970fa8b356d0ed8af960c77edf0c2f4d0e4d0d
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-13 13:10:33 +0200

    test client componet -> success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-const Page = async () => {
-  return <div className="">Hello world!</div>;
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+
+  const { data } = useQuery(
+    trpc.hello.queryOptions({
+      text: "Bob!",
+    })
+  );
+
+  return (
+    <div className="">
+      Hello world!
+      <div className="">{JSON.stringify(data)}</div>
+    </div>
+  );
 };
 
 export default Page;
