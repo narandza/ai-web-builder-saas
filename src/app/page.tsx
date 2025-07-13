be7af2bbb97bd5fa8a20ef353a34a05848460c7b
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-13 12:27:21 +0200

    test prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-const Page = () => {
-  return <div className="text-rose-100">Hello world!</div>;
+import { prisma } from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
+  return <div className="">{JSON.stringify(users, null, 2)}</div>;
 };
 
 export default Page;
