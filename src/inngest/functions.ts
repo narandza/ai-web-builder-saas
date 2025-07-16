8c4c13c188f65403544f64fae4fbf05be5ac8abb
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-16 12:46:41 +0200

    build agent snippet

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { inngest } from "./client";
+import { createAgent, openai } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
+  { id: "code-agent" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-next-js-devmitrije010");
+
+      return sandbox.sandboxId;
+    });
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert. next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets",
+      model: openai({ model: "gpt-4o" }),
+    });
+  }
 );
