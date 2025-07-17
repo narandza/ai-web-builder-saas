32eb7f4b4164d2635638523c4e04765e1c3e1216
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-17 10:34:43 +0200

    build terminal tool

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,8 +1,9 @@
 import { Sandbox } from "@e2b/code-interpreter";
-import { createAgent, openai } from "@inngest/agent-kit";
+import { createAgent, createTool, openai } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
 import { getSandbox } from "./utils";
+import z from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "code-agent" },
@@ -19,6 +20,41 @@ export const helloWorld = inngest.createFunction(
       system:
         "You are an expert. next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets",
       model: openai({ model: "gpt-4o" }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+
+              try {
+                const sandbox = await getSandbox(sandboxId);
+
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                });
+
+                return result.stdout;
+              } catch (e) {
+                console.error(
+                  `Command failed: ${e} \nstoud: ${buffers.stdout}\nstderror: ${buffers.stderr}`
+                );
+
+                return `Command failed: ${e} \nstoud: ${buffers.stdout}\nstderror: ${buffers.stderr}`;
+              }
+            });
+          },
+        }),
+      ],
     });
 
     const { output } = await codeAgent.run(
