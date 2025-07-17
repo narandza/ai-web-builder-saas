2584358fc49298f3519a892f0efc082e3960e7fd
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-17 12:41:10 +0200

    update page.tsx update app router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions());
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
   return (
-    <div className="">
-      <Button onClick={() => invoke.mutate({ text: "John" })}>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value: value })}>
         Invoke background job
       </Button>
     </div>
