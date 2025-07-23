ca21e43116cd040de3e214074084aa9d7cbd51d3
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-23 08:14:48 +0200

    build messages containet component

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -7,6 +7,8 @@ import {
 } from "@/components/ui/resizable";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { MessagesContainer } from "../components/messages-container";
+import { Suspense } from "react";
 
 interface Props {
   projectId: string;
@@ -19,10 +21,6 @@ export const ProjectView = ({ projectId }: Props) => {
     trpc.projects.getOne.queryOptions({ id: projectId })
   );
 
-  const { data: messages } = useSuspenseQuery(
-    trpc.messages.getMany.queryOptions({ projectId })
-  );
-
   return (
     <div className="h-screen">
       <ResizablePanelGroup direction="horizontal">
@@ -31,7 +29,9 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
-          TODO: Messages Container
+          <Suspense fallback={<p>Loading messages...</p>}>
+            <MessagesContainer projectId={projectId} />
+          </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={65} minSize={50}>
