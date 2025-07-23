41a50fd4ea947ec5e809907bbdcd935ef6394008
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-23 08:50:03 +0200

    build assistand message component

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -1,5 +1,45 @@
 import { Card } from "@/components/ui/card";
 import { Fragment, MessageRole, MessageType } from "@/generated/prisma";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
+interface AssistantMessageProps {
+  content: string;
+  fragment: Fragment | null;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: MessageType;
+}
+
+const AssistantMessage = ({
+  content,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: AssistantMessageProps) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col group px-2 pb-4",
+        type === "ERROR" && "text-red-700 dark:text-red-500"
+      )}
+    >
+      <div className="flex items-center gap-2 pl-2 mb-2">
+        {/* Add logo */}
+        <span className="text-sm font-medium">Vibe</span>
+        <span className="text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
+          {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
+        </span>
+      </div>
+      <div className="pl-8.5 flex flex-col gap-y-4">
+        <span className="">{content}</span>
+      </div>
+    </div>
+  );
+};
 
 interface UserMessageProps {
   content: string;
@@ -32,9 +72,18 @@ export const MessageCard = ({
   isActiveFragment,
   onFragmentClick,
   type,
-}) => {
+}: Props) => {
   if (role === "ASSISTANT") {
-    return <p className="">assistant</p>;
+    return (
+      <AssistantMessage
+        content={content}
+        fragment={fragment}
+        createdAt={createdAt}
+        isActiveFragment={isActiveFragment}
+        onFragmentClick={onFragmentClick}
+        type={type}
+      />
+    );
   }
   return <UserMessage content={content} />;
 };
