f20bee927fd663146653a59cfedf0918fce1bbd9
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-23 07:56:39 +0200

    build project view ui with resizable panels

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
@@ -19,9 +24,20 @@ export const ProjectView = ({ projectId }: Props) => {
   );
 
   return (
-    <div className="">
-      {JSON.stringify(project)}
-      {JSON.stringify(messages)}
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          TODO: Messages Container
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+          TODO: Preview
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
