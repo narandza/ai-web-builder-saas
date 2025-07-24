24924bf211e395f428f2d11c25f9fa30ea135c89
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-24 14:00:00 +0200

    build messages interval function

--- src/modules/projects/ui/components/message-loading.tsx
+++ src/modules/projects/ui/components/message-loading.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const ShimmerMessages = () => {
   const messages = [
     "Thinking...",
@@ -10,6 +12,16 @@ const ShimmerMessages = () => {
     "Adding final touches",
     "Almost ready...",
   ];
+
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMessageIndex((prev) => (prev + 1) % messages.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [messages.length]);
 };
 
 export const MessageLoading = () => {
