095a43b552874d90d43988ad5f5c81c290139726
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-24 07:25:16 +0200

    build form ui

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,6 +1,10 @@
 import { useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField } from "@/components/ui/form";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import TextareaAutosize from "react-textarea-autosize";
 
 interface Props {
   projectId: string;
@@ -14,11 +18,53 @@ const formSchema = z.object({
 });
 
 export const MessageForm = ({ projectId }: Props) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const showUsage = false;
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       value: "",
     },
   });
-  return <div className=""></div>;
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+          isFocused && "shadow-xs",
+          showUsage && "rounded-t-none"
+        )}
+      >
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <TextareaAutosize
+              {...field}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              minRows={2}
+              maxRows={8}
+              className="pt-4 resize-none border-none w-full outline-none bg-transparent"
+              placeholder="What would yo like to build"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                  e.preventDefault();
+                  form.handleSubmit(onSubmit)(e);
+                }
+              }}
+            />
+          )}
+        />
+      </form>
+    </Form>
+  );
 };
