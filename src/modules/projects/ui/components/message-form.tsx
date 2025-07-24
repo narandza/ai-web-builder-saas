456469ba52bfb049f1a0ff06506bdfffbd685f6d
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-24 07:12:25 +0200

    build form logic

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,7 +1,24 @@
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 interface Props {
   projectId: string;
 }
 
+const formSchema = z.object({
+  value: z
+    .string()
+    .min(1, { message: "Value is required" })
+    .max(10_000, { message: "Value is too long" }),
+});
+
 export const MessageForm = ({ projectId }: Props) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
   return <div className=""></div>;
 };
