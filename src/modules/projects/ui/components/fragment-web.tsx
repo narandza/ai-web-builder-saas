353b147ce9901cadbb26544260eddba94b314c45
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-29 17:13:10 +0200

    add fragment key logic

--- src/modules/projects/ui/components/fragment-web.tsx
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -1,24 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { Fragment } from "@/generated/prisma";
 import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
+import { useState } from "react";
 
 interface Props {
   data: Fragment;
 }
 
 export function FragmentWeb({ data }: Props) {
+  const [copied, setCopied] = useState(false);
+  const [fragmentKey, setFragmentKey] = useState(0);
+
+  const onRefresh = () => {
+    setFragmentKey((prev) => prev + 1);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.sandboxUrl);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
-        <Button size="sm" variant="outline" onClick={() => {}}>
+        <Button size="sm" variant="outline" onClick={onRefresh}>
           <RefreshCcwIcon />
         </Button>
         <Button
           size="sm"
           variant="outline"
-          onClick={() => {}}
+          onClick={handleCopy}
           className="flex-1 justify-start text-start font-normal"
-          disabled={false}
+          disabled={!data.sandboxUrl || copied}
         >
           <span className="truncate">{data.sandboxUrl}</span>
         </Button>
@@ -35,6 +49,7 @@ export function FragmentWeb({ data }: Props) {
         </Button>
       </div>
       <iframe
+        key={fragmentKey}
         className="h-full  w-full"
         sandbox="allow-forms allow-scripts allow-same-origin"
         loading="lazy"
