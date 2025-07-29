54872a6c6f7bec0d6a14fc6a46b50f0fbbe75aec
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-29 17:08:29 +0200

    build fragment header

--- src/modules/projects/ui/components/fragment-web.tsx
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Fragment } from "@/generated/prisma";
+import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
 
 interface Props {
   data: Fragment;
@@ -7,6 +9,31 @@ interface Props {
 export function FragmentWeb({ data }: Props) {
   return (
     <div className="flex flex-col w-full h-full">
+      <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
+        <Button size="sm" variant="outline" onClick={() => {}}>
+          <RefreshCcwIcon />
+        </Button>
+        <Button
+          size="sm"
+          variant="outline"
+          onClick={() => {}}
+          className="flex-1 justify-start text-start font-normal"
+          disabled={false}
+        >
+          <span className="truncate">{data.sandboxUrl}</span>
+        </Button>
+        <Button
+          size="sm"
+          disabled={!data.sandboxUrl}
+          variant="outline"
+          onClick={() => {
+            if (!data.sandboxUrl) return;
+            window.open(data.sandboxUrl, "_blank");
+          }}
+        >
+          <ExternalLinkIcon />
+        </Button>
+      </div>
       <iframe
         className="h-full  w-full"
         sandbox="allow-forms allow-scripts allow-same-origin"
