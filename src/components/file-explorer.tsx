0f8ffec86db1dc058b822d9052be738903956730
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-30 19:33:28 +0200

    Add code view

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -4,6 +4,10 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "./ui/resizable";
+import { Hint } from "./hint";
+import { Button } from "./ui/button";
+import { CopyIcon } from "lucide-react";
+import { CodeView } from "./code-view";
 
 type FileCollection = { [path: string]: string };
 
@@ -30,8 +34,27 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
       <ResizableHandle className="hover:bg-primary transition-colors" />
       <ResizablePanel defaultSize={70} minSize={50} className="bg-sidebar">
         {selectedFile && files[selectedFile] ? (
-          <div className="">
-            <p>TODO view</p>
+          <div className="h-full w-full flex flex-col">
+            <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
+              {/* TODO: File breadcrumb */}
+              <Hint text="Copy to clipboard" side="bottom">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="ml-auto"
+                  onClick={() => {}}
+                  disabled={false}
+                >
+                  <CopyIcon />
+                </Button>
+              </Hint>
+            </div>
+            <div className="flex-1 overflow-auto">
+              <CodeView
+                code={files[selectedFile]}
+                lang={getLanguageFromExtension(selectedFile)}
+              />
+            </div>
           </div>
         ) : (
           <div className="flex h-full items-center justify-center9 text-muted-foreground">
