a3f459b33733918afba498b2babad0d5c87105bf
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-07-31 20:33:17 +0200

    build Treemfile component

--- src/components/tree-view.tsx
+++ src/components/tree-view.tsx
@@ -5,8 +5,10 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarProvider,
 } from "./ui/sidebar";
+import { FileIcon } from "lucide-react";
 
 interface TreeViewProps {
   data: TreeItem[];
@@ -28,4 +30,34 @@ export const TreeView = ({ data, value, onSelect }: TreeViewProps) => {
       </Sidebar>
     </SidebarProvider>
   );
+};
+
+interface TreeProps {
+  item: TreeItem;
+  selectedValue?: string | null;
+  onSelect?: (value: string) => void;
+  parentPath: string;
+}
+
+const Tree = ({ item, selectedValue, onSelect, parentPath }: TreeProps) => {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+
+  const currentPath = parentPath ? `${parentPath}/name` : name;
+
+  if (!items.length) {
+    // It's a file
+    const isSelected = selectedValue === currentPath;
+
+    return (
+      <SidebarMenuButton
+        isActive={isSelected}
+        className="data-[active=true]:bg-transparent"
+        onClick={() => onSelect?.(currentPath)}
+      >
+        <FileIcon />
+        <span className="truncate">{name}</span>
+      </SidebarMenuButton>
+    );
+  }
+  // TODO: Build when it's a folder
 };
