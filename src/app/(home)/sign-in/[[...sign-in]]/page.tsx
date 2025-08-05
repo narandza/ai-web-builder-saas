dbe0f61c51e056b63b4b9cbcdcdbb0ff094aa770
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-05 21:01:36 +0200

    modify sign up page

--- src/app/(home)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(home)/sign-in/[[...sign-in]]/page.tsx
@@ -6,6 +6,7 @@ import { dark } from "@clerk/themes";
 
 const Page = () => {
   const currentTheme = useCurrentTheme();
+
   return (
     <div className="flex flex-col max-w-3xl mx-auto w-full">
       <section className="space-y-6 pt-[16vh] 2xl:pt-48">
