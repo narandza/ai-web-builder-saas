032c29dad6c52573eec325da0d6fe08be5471d39
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-06 20:11:13 +0200

    update ui for pro access

--- src/modules/projects/ui/components/usage.tsx
+++ src/modules/projects/ui/components/usage.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/nextjs";
 import { formatDuration, intervalToDuration } from "date-fns";
 import { CrownIcon } from "lucide-react";
 import Link from "next/link";
@@ -9,11 +10,19 @@ interface Props {
 }
 
 export const Usage = ({ points, msBeforeNext }: Props) => {
+  const { has } = useAuth();
+
+  const hasProAccess = has?.({
+    plan: "pro",
+  });
+
   return (
     <div className="rounded-t-xl bg-background border border-b-0 p-2.5">
       <div className="flex items-center gap-x-2">
         <div>
-          <p className="text-sm">{points} free credits remaining</p>
+          <p className="text-sm">
+            {points} {hasProAccess ? "" : "free"} credits remaining
+          </p>
           <p className="text-xs text-muted-foreground">
             Resets in{" "}
             {formatDuration(
@@ -25,11 +34,13 @@ export const Usage = ({ points, msBeforeNext }: Props) => {
             )}
           </p>
         </div>
-        <Button asChild size="sm" variant="tertiary" className="ml-auto">
-          <Link href="/pricing">
-            <CrownIcon /> Upgrade
-          </Link>
-        </Button>
+        {!hasProAccess && (
+          <Button asChild size="sm" variant="tertiary" className="ml-auto">
+            <Link href="/pricing">
+              <CrownIcon /> Upgrade
+            </Link>
+          </Button>
+        )}
       </div>
     </div>
   );
