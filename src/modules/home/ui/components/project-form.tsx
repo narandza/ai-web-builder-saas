d4744c9f49e044ad16e430d65f469757cba6a6a4
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-06 20:02:23 +0200

    invalidate + error handle in projects form

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -35,14 +35,16 @@ export const ProjectForm = () => {
       onSuccess: (data) => {
         queryClient.invalidateQueries(trpc.projects.getMany.queryOptions());
         router.push(`/projects/${data.id}`);
-        // TODO: Invalidate useage status
+        queryClient.invalidateQueries(trpc.usage.status.queryOptions());
       },
       onError: (error) => {
         toast.error(error.message);
         if (error.data?.code === "UNAUTHORIZED") {
           clerk.openSignIn();
         }
-        // TODO: redirect to pricing page if specific error
+        if (error.data?.code === "TOO_MANY_REQUESTS") {
+          router.push("/pricing");
+        }
       },
     })
   );
