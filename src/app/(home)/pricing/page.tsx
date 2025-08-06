eaa6abdda3331ab39af471eb8eaa786154b855f5
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-06 18:12:28 +0200

    add dark mode pricing support

--- src/app/(home)/pricing/page.tsx
+++ src/app/(home)/pricing/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useCurrentTheme } from "@/hooks/use-current-theme";
 import { PricingTable } from "@clerk/nextjs";
 import Image from "next/image";
 
 const Page = () => {
+  const currentTheme = useCurrentTheme();
+
   return (
     <div className="flex flex-col max-w-3xl mx-auto w-full">
       <section className="space-y-6 pt-[16vh] 2xl:pt-48">
@@ -22,6 +25,7 @@ const Page = () => {
         </div>
         <PricingTable
           appearance={{
+            baseTheme: currentTheme === "dark" ? dark : undefined,
             elements: {
               pricingTableCard: "border! shadow-none! rounded-lg!",
             },
