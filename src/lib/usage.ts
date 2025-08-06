91b5fa01e5b8d50c3ede0e1c684534c912d30aa4
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-06 19:28:29 +0200

    build consume credits function

--- src/lib/usage.ts
+++ src/lib/usage.ts
@@ -1,9 +1,10 @@
 import { RateLimiterPrisma } from "rate-limiter-flexible";
 import { prisma } from "./db";
+import { auth } from "@clerk/nextjs/server";
 
 const FREE_POINTS = 5;
-
-const DURATION = 30 * 24 * 60 * 60; // 30 day
+const DURATION = 30 * 24 * 60 * 60; // 30 days
+const GENERATION_COST = 1;
 
 export async function getUsageTracker() {
   const usageTracker = new RateLimiterPrisma({
@@ -14,4 +15,18 @@ export async function getUsageTracker() {
   });
 
   return usageTracker;
+}
+
+export async function consumeCredits() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const usageTracker = await getUsageTracker();
+
+  const result = await usageTracker.consume(userId, GENERATION_COST);
+
+  return result;
 }
