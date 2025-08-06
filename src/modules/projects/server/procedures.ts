e28f8e9ce809b2abb7392b5c478de9143e2dcc11
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-06 20:00:52 +0200

    consume credits when creating a new project

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -5,6 +5,7 @@ import { generateSlug } from "random-word-slugs";
 import { prisma } from "@/lib/db";
 import { inngest } from "@/inngest/client";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
+import { consumeCredits } from "@/lib/usage";
 
 export const projectsRouter = createTRPCRouter({
   getOne: protectedProcedure
@@ -54,6 +55,22 @@ export const projectsRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input, ctx }) => {
+      try {
+        await consumeCredits();
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Something went wrong",
+          });
+        } else {
+          throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "You have run out of credits",
+          });
+        }
+      }
+
       const createdProject = await prisma.project.create({
         data: {
           userId: ctx.auth.userId,
